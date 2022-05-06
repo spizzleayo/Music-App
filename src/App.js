769fb55e6caa382a981815ad06a844ccf79ea769
RAJI <[rajiayomikun@gmail.com]>
2022-05-06 16:18:13 +0000

    update and added scss support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
-
+import React, {useState} from "react";
+//import styles
+import './styles/app.scss'
+//addin components
+import Player from './components/Player';
+import Song from './components/Song';
+//import util
+import data from './util';
 
 function App() {
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-        <h1>Music Player</h1>
+        <Song currentSong={currentSong} />
+        <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying} 
+        currentSong={currentSong} />
     </div>
   );
 }
---
+++ src/components/Player.js
@@ -0,0 +1,61 @@
+import React, {useRef, useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { 
+     faPlay,
+     faAngleLeft, 
+     faAngleRight,
+     } from "@fortawesome/free-solid-svg-icons";
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+    //Ref
+        const audioRef = useRef(null);
+
+    //event handers
+    const playSongHandler= () => {
+       if (isPlaying)  {
+           audioRef.current.pause();
+           setIsPlaying(!isPlaying);
+       }else{
+           audioRef.current.play();
+           setIsPlaying(!isPlaying);
+       }
+    };
+    const timeUpdateHandler = (e) => {
+       const current = e.target.currentTime;
+       const duration = e.target.duration;
+       setSongInfo({...songInfo, currentTime: current, duration})
+    };
+    const getTime = (time) => {
+        return(
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2) 
+        );
+    }
+    //state
+    const [songInfo, setSongInfo] = useState({
+        currentTime: null,
+        duration: null,
+    });
+    return(
+        <div className="player">
+          <div className="time-control">
+              <p>{getTime(songInfo.currentTime)}</p>
+              <input type="range"/>
+              <p>{getTime(songInfo.duration)}</p>
+          </div> 
+          <div className="play-control">
+             <FontAwesomeIcon className="backward" size="2x"icon={faAngleLeft} />
+             <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
+             <FontAwesomeIcon className="forward" size="2x" icon={faAngleRight} />
+    
+          </div>
+          <audio 
+          onTimeUpdate={timeUpdateHandler} 
+          onLoadedMetadata={timeUpdateHandler}
+          ref={audioRef} 
+          src={currentSong.audio}>
+
+          </audio>
+        </div>
+    );
+};
+
+export default Player;
